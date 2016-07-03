67bad3bf13dc4d9918778cea3b62602fd4e73e47
zo17 <juliana.leon@global.tesco.org>
2016-07-03 14:00:43 +0100

    prep repo for heroku

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ var express = require('express');
 //Creat the APP
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(8000, function(){
-  console.log('Express server is up on port 8000');
+
+app.listen(PORT, function(){
+  console.log('Express server is up on port' + PORT);
 });
